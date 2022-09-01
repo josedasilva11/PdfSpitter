ec90d0111b89a859ce654feb884ee2422cb7265d
Ender <sterlingkooshesh@gmail.com>
2022-09-01 13:37:33 -0500

    fucking python strings

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = Flask('applicant', static_url_path='',
             template_folder='templates')
 app.config.update(SECRET_KEY=secrets.token_hex() )
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///applicants.db'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://vnmeonlg:seLb8IF2H_uExLnYDGau9sFM4bKCsVkX@kesavan.db.elephantsql.com/vnmeonlg'
+with open('database.txt') as db:
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://'  + db.read().strip()
 app.config['SQLALCHEMY_POOL_SIZE'] = 2
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
